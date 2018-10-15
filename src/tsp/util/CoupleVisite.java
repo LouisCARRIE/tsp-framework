7fbd1b3b3e2af3b6f0c1d667357ad5d372ae3efd
Thomas <Thomas@LAPTOP-ARE9N4SJ>
2018-10-15 10:55:33 +0200

    couplevisite

---
+++ src/tsp/util/CoupleVisite.java
@@ -0,0 +1,25 @@
+package tsp.util;
+
+public class CoupleVisite {
+	double phero;
+	boolean b;
+	public CoupleVisite() {
+		this(0, false);
+	}
+	public CoupleVisite(double phero, boolean b) {
+		this.phero = phero;
+		this.b = b;
+	}
+	public double getPhero() {
+		return phero;
+	}
+	public void setPhero(double phero) {
+		this.phero = phero;
+	}
+	public boolean getB() {
+		return b;
+	}
+	public void setB(boolean b) {
+		this.b = b;
+	}
+}
