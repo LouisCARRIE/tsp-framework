9c3c881f5260bf44d741f1e0c7883902c9f9a58f
Brendan <Brendan@172.18.224.144>
2018-10-15 11:04:07 +0200

    coloniedefourmi

--- src/tsp/heuristic/ColonieDeFourmis.java
+++ src/tsp/heuristic/ColonieDeFourmis.java
@@ -1,7 +1,9 @@
 package tsp.heuristic;
 
 import tsp.Instance;
-import java.util.ArrayList; 
+import tsp.util.CoupleVisite;
+
+
 
 public class ColonieDeFourmis extends AHeuristic {
 
@@ -10,21 +12,33 @@ public class ColonieDeFourmis extends AHeuristic {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void solve() throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
+	
 	public static double phenoromone ( double pheno, double longeurChemin) {
 		int G=1;
 		return (pheno+G/longeurChemin);
 	}
 	
-	ArrayList L = new ArrayList<>(5);
+	public static boolean convergence () {
+		return true;
+	}
 	
 	
+	@Override
+	public void solve() throws Exception {
+		// TODO Auto-generated method stub
+		int nombreFourmis=100;
+		int quantitePhero=100;
+		CoupleVisite villes = new CoupleVisite();
+		while (quantitePhero>0|| convergence()==true) {
+			for (int k=0; k < nombreFourmis; k++) {
+				
+				
+				
+			}
+		}
 		
 	}
 	
-
 }
